c726f6a34f77ac7df5ceffabf2a85e1803cd9a9f
EKirgizov <kirgizov.evgeni.1985@gmail.com>
2022-02-28 02:43:24 +0700

    исправил недостатки

--- js/main.js
+++ js/main.js
@@ -3,29 +3,31 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
 
   if (max>min) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;}
+    return Math.floor(Math.random() * (max - min + 1)) + min;}
   else {
-    if (min=max) {
+    if (min===max) {
       return min;
-    } else {return 'Значение От больше значения До'}
+    } else {return 'Значение От больше значения До';}
   }
 }
+
+getRandomIntInclusive(1,4);
 
 //источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 
 function getRandomInclusive(min, max, afterPoint) {
-
-
   if (max>min) {
-    return (min + Math.random() * (max - min)).toFixed(afterPoint)}
-    else {
-      if (min=max) {
-        return min.toFixed(afterPoint);
-      } else {
-        return 'Значение От больше значения До'
-      }
+    return (min + Math.random() * (max - min)).toFixed(afterPoint);}
+  else {
+    if (min===max) {
+      return min.toFixed(afterPoint);
+    } else {
+      return 'Значение От больше значения До';
     }
+  }
 }
+
+getRandomInclusive(1.1,1.5,3);
 
 //https://learn.javascript.ru/number
